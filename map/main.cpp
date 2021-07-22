fa4baf62dc3a5e87d41bb251a37bd97b6ef7964a
avan-pra <velovo.vanpraet@gmail.com>
2021-07-22 16:11:12 +0200

    map initial commit

---
+++ map/main.cpp
@@ -0,0 +1,8 @@
+#include "vector.hpp"
+#include <map>
+#include <iostream>
+
+int             main(void)
+{
+	std::map<int, int> m;
+}
---
+++ map/map.hpp
@@ -0,0 +1,59 @@
+#ifndef MAP_HPP
+# define MAP_HPP
+
+# include <cstdlib>
+# include <cstddef>
+# include <memory>
+# include <climits>
+# include <sstream>
+# include <iostream>
+# include "../utils/utils.hpp"
+# include "iterator.hpp"
+# include "reverse_iterator.hpp"
+
+namespace ft
+{
+
+	template <
+			class Key,
+			class T,
+			class Compare = std::less<Key>,
+			class Alloc = std::allocator<T>
+			// class Alloc = std::allocator<pair<const Key,T> >    // map::allocator_type 
+			>
+	class map
+	{
+		public:
+			typedef Key									key_type;
+			typedef T									mapped_type;
+			typedef pair<const key_type, mapped_type>	value_type;
+			typedef Compare								key_compare;
+			//	value_compare ?????
+			typedef Alloc								allocator_type;
+			typedef value_type							&reference;
+			typedef const value_type					&const_reference;
+			typedef value_type							*pointer;
+			typedef const value_type					*const_pointer;
+
+
+			typedef ptrdiff_t							difference_type;
+			typedef size_t								size_type;
+			
+
+		private:
+			Alloc			_alloc;
+			pointer			_ptr;
+			size_type		_size;
+			size_type		_size_alloc;
+
+		public:
+
+
+			allocator_type get_allocator() const
+			{
+				return _alloc;
+			}
+	};
+}
+
+#endif
