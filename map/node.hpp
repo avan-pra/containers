37d7d6954d8aa3b54905f29d5ce91e9a08f12ee6
avan-pra <velovo.vanpraet@gmail.com>
2021-07-26 17:20:45 +0200

    refactor node + rename

---
+++ map/node.hpp
@@ -0,0 +1,23 @@
+#ifndef BTREE_HPP
+# define BTREE_HPP
+
+template <class T>
+struct node
+{
+	public:
+		typedef T		value_type;
+		value_type	data;
+		struct node	*left;
+		struct node	*right;
+		struct node	*parent;
+
+		node();
+		node(const value_type &val = value_type()) : data(val), left(NULL), right(NULL), parent(NULL) { }
+		~node() { }
+		node(const node &node) : data(node.data), left(node.left), right(node.right), parent(node.parent) { }
+		node(const value_type &val, node *depend) : data(val), left(NULL), right(NULL), parent(depend) { }
+		node &operator=(const node &node) : data(node.data), left(node.left), right(node.right), parent(node.parent) { return *this; }
+		value_type operator->() { return data; }
+};
+
+#endif
