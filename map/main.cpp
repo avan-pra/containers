0a5c482d5012be214129e1802229cbb8e2b6dd2c
avan-pra <velovo.vanpraet@gmail.com>
2021-09-03 17:58:26 +0200

    add main for testing

--- map/main.cpp
+++ map/main.cpp
@@ -4,6 +4,13 @@
 
 int             main(void)
 {
+	// test1212 bjr;
+
 	ft::map<int, int> m;
+	m.insert(ft::make_pair(1, 1));
+	m.insert(ft::make_pair(2, 2));
+	m.insert(ft::make_pair(3, 3));
+	m.insert(ft::make_pair(0, 72));
+
 	m.test();
 }
