41e789d77a5a4b54a441519cfd1c0be0a4a976cf
avan-pra <velovo.vanpraet@gmail.com>
2021-09-16 18:53:10 +0200

    main de test

--- map/main.cpp
+++ map/main.cpp
@@ -6,21 +6,42 @@ int             main(void)
 {
 	// test1212 bjr;
 
-	std::map<int, int> m;			//tree is from: https://stackoverflow.com/questions/2942517/how-do-i-iterate-over-binary-tree
-	m.insert(std::make_pair(8, 1));
-	m.insert(std::make_pair(3, 1));
-	m.insert(std::make_pair(10, 1));
-	m.insert(std::make_pair(14, 1));
-	m.insert(std::make_pair(13, 1));
-	m.insert(std::make_pair(1, 1));
-	m.insert(std::make_pair(6, 1));
-	m.insert(std::make_pair(4, 1));
-	m.insert(std::make_pair(7, 1));
+	ft::map<int, int> m;			//tree is from: https://stackoverflow.com/questions/2942517/how-do-i-iterate-over-binary-tree
+	m.insert(ft::make_pair(8, 1));
+	m.insert(ft::make_pair(3, 1));
+	m.insert(ft::make_pair(10, 1));
+	m.insert(ft::make_pair(14, 1));
+	m.insert(ft::make_pair(13, 1));
+	m.insert(ft::make_pair(1, 1));
+	m.insert(ft::make_pair(6, 2));
+	m.insert(ft::make_pair(4, 1));
+	m.insert(ft::make_pair(7, 1));
+	m.insert(ft::make_pair(-1, 1));
 
-	std::map<int, int>::iterator it = m.begin();
-	std::map<int, int>::iterator it2 = m.begin();
-	it--;
-	std::cout << (it == it2) << std::endl;
+
+	m.clear();
+	m.insert(ft::make_pair(10, 1));
+	
+	
+	// m[25] = 12;
+	// m[26] = 13;
+
+	ft::map<int, int>::iterator it;
+	// ft::map<int, int>::iterator it = m.begin();
+	// std::cout << it->second << std::endl;
+	// m.erase(it);
+
+	//std::cout << m.erase(8) << std::endl;
+	for (it = m.begin(); it != m.end(); ++it)
+	{
+		std::cout << it->first << std::endl;
+	}
+	std::cout << "yo" << std::endl;
+	// ft::map<int, int>::iterator it = m.begin();
+	// ft::map<int, int>::iterator it2 = m.begin();
+	// --it;
+	// std::cout << (it == it2) << std::endl;
+	// std::cout << it->first << std::endl;
 	// ft::cout << it->first << std::endl;
 	// ft::map<int, int>::iterator it = m.begin();
 
@@ -42,8 +63,6 @@ int             main(void)
 	// ++it;
 	// std::cout << it->first << std::endl;
 	// ++it;
-	// --it;
-	// std::cout << it->first << std::endl;
 
 	// --it;
 	// m.test();
