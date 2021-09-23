d1432f132989b10054520b672f6c572e79aa5743
avan-pra <velovo.vanpraet@gmail.com>
2021-09-23 16:54:13 +0000

    modify namespace for integral and add lexicographical

--- utils/utils.hpp
+++ utils/utils.hpp
@@ -35,6 +35,42 @@ namespace utils
 		a = b;
 		b = c;
 	}
+}
+
+namespace ft
+{
+	template <class InputIterator1, class InputIterator2>
+	bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1,
+									InputIterator2 first2, InputIterator2 last2)
+	{
+		while (first1 != last1)
+		{
+			if (first2 == last2 || *first2 < *first1)
+				return false;
+			else if (*first1 < *first2)
+				return true;
+			++first1;
+			++first2;
+		}
+		return (first2!=last2);
+	}
+
+	template <class InputIterator1, class InputIterator2, class Compare>
+	bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1,
+								InputIterator2 first2, InputIterator2 last2,
+								Compare comp)
+	{
+		while (first1 != last1)
+		{
+			if (first2 == last2 || Comp(*first2, *first1))
+				return false;
+			else if (Comp(*first1, *first2))
+				return true;
+			++first1;
+			++first2;
+		}
+		return (first2!=last2);
+	}
 
 	//is_integral implementation
 	template <bool is_integral, typename T>
