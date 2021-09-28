8b210c677262f90b48004531f78f85fd32458447
avan-pra <velovo.vanpraet@gmail.com>
2021-09-28 15:01:28 +0000

    same

--- utils/utils.hpp
+++ utils/utils.hpp
@@ -3,40 +3,6 @@
 
 #include <iostream>
 
-namespace utils
-{
-	template <typename T>
-	std::string		ft_to_string(T value)
-	{
-		std::string output;
-		std::string sign;
-		char		nb[2];
-
-		if (value < 0)
-		{
-			sign + "-";
-			value = -value;
-		}
-		nb[1] = '\0';
-		while (output.empty() || (value > 0))
-		{
-			nb[0] = value % 10 + '0';
-			output.insert(0, std::string(nb));
-			value /= 10;
-		}
-
-		return (sign + output);
-	}
-
-	template <class T>
-	void swap(T &a, T &b)
-	{
-		T c = a;
-		a = b;
-		b = c;
-	}
-}
-
 namespace ft
 {
 	template <class InputIterator1, class InputIterator2>
